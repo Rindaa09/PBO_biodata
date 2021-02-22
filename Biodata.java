4cdb5eb5e9c5d52f774684cecf16cbbe788c2a06
Rindaa09 <74225747+Rindaa09@users.noreply.github.com>
2021-02-22 08:24:53 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rindaaa;
+
+/**
+ *
+ * @author USER
+ */
+public class Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama="Rinda Lestari";
+        String kelas="XI RPL 2";
+        int absen=27;
+        String jurusan="Rekayasa Perangkat Lunak";
+        String alamat="Kucur Sumberrejo";
+        String tempat_lahir="Pasuruan";
+        String tanggal_lahir="09 Juli 2003";
+        
+        System.out.println("Biodata Siswa");
+        System.out.println("Nama \t\t\t\t\ :"+nama);
+        System.out.println("Kelas \t\t\t\t\ :"+kelas);
+        System.out.println("Absen \t\t\t\t\ :"+absen);
+        System.out.println("Jurusan \t\t\t\ :"+jurusan);
+        System.out.println("Alamat \t\t\t\t\ :"+alamat);
+        System.out.println("Tempat Lahir \t\t\t\ :"+tempat_lahir;
+        System.out.println("Tanggal Lahir \t\t\t\ :"+tanggal_lahir);
+        
+        
+        
+        
+    }
+    
+}
